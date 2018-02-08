543819a088d2aa32297994263d9d0964908df4ae
Di Wu <allwu@falcon-computing.com>
2018-02-08 11:21:07 -0800

    minor update for release.sh

--- release/release.sh
+++ release/release.sh
@@ -7,9 +7,9 @@ mkdir -p $repo_dir
 
 # check versions
 suite_version=v1.1.2
-bwa_version=v0.4.0-dist
+bwa_version=v0.4.0-6-dist
 gatk_version=3.8-falcon-v0.4.1
-release_version=v0.1.0-test
+release_version=v0.1.0-ucla-garon
 
 locate_file() {
   local file=$1;
@@ -40,10 +40,10 @@ copy_file "fcs-genome/fcs-genome-${suite_version}" falcon/bin/fcs-genome
 copy_file "bwa/bwa-${bwa_version}" falcon/tools/bin/bwa-bin
 copy_file "gatk/GATK-${gatk_version}.jar" falcon/tools/package/GenomeAnalysisTK.jar
 
-tar zcfh falcon-genome-${release_version}.tgz falcon/
+tar pzcfh falcon-genome-${release_version}.tgz falcon/
 
 # export to s3
-aws s3 cp falcon-genome-${release_version}.tgz s3://fcs-genome-pub/release/
+aws s3 cp falcon-genome-${release_version}.tgz s3://fcs-genome-pub/release/ --acl public-read
 #aws s3 cp falcon-genome-${release_version}.tgz s3://fcs-genome-pub/release/falcon-genome-latest.tgz
 
 rm -rf falcon
