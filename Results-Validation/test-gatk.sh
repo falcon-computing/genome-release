65f7f751aada279d107c9fd0efd7b17c677e04c6
Niveda-S <nivedasundar05@gmail.com>
2018-02-21 12:13:21 -0800

    Support individual GATK steps

--- Results-Validation/test-gatk.sh
+++ Results-Validation/test-gatk.sh
@@ -2,13 +2,57 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 source $DIR/globals.sh
 
-if [[ $# -ne 2 ]];then
-  echo "USAGE: $0 [gatk.jar] [ID list]"
-  exit 1
+print_help() {
+  echo "USAGE:"
+  echo "$0 \\";
+  echo "-g <gatk.jar> \\";
+  echo "-i <ID list> \\";
+  echo "-b <bqsr> \\";
+  echo "-p <print reads> \\";
+  echo "-h <haplotype caller> \\";
+  echo "-a <all>"
+}
+
+if [[ $# -lt 2 ]];then
+  print_help
+  exit 1;
 fi
 
-gatk=$1
-data_list=$2
+while [[ $# -gt 0 ]];do
+  key="$1"
+  case $key in
+  -g|--gatk)
+    gatk="$2"
+    shift
+  ;;
+  -i|--list)
+    data_list="$2"
+    shift
+  ;;
+  -b|--bqsr)
+    bqsr=1
+    shift
+  ;;
+  -p|--pr)
+    pr=1
+    shift
+  ;;
+  -h|--htc)
+    htc=1
+    shift
+  ;;
+  -a|--all)
+    all=1
+    shift
+  ;;
+  *)
+  echo "Failed to recognize argument '$1'"
+  print_help
+  exit 1
+  ;;
+  esac
+  shift
+done
 
 gatk_version=$(java -jar $gatk --version)
 echo "GATK version $gatk_version"
@@ -30,8 +74,10 @@ baselines=$temp/baselines
 mkdir -p $baselines
 mkdir -p $baselines/$id
 
-aws s3 cp --recursive s3://fcs-genome-data/baselines/$id/ $baselines/$id
+#aws s3 cp --recursive s3://fcs-genome-data/baselines/$id/ $baselines/$id
 
+if [[ $bqsr == 1 || $all == 1 ]];then
+ 
 #Base Recalibration
 java -jar $gatk -T BaseRecalibrator \
         -R $ref_genome \
@@ -40,11 +86,17 @@ java -jar $gatk -T BaseRecalibrator \
         -knownSites $db138_SNPs \
         -knownSites $g1000_indels \
         -knownSites $g1000_gold_standard_indels \
-        -nct 8 
+        -nct 1 
 
 if [[ $? -ne 0 ]];then
 echo "Failed base recalibration"
 fi
+
+BQSR=$($DIR/compare_BQSR.sh ${temp_dir}/${id}_BQSR.table $id)
+
+fi
+
+if [[ $pr == 1 || $all == 1 ]];then
 
 #Print Reads
 java -jar $gatk -T PrintReads \
@@ -52,72 +104,41 @@ java -jar $gatk -T PrintReads \
         -BQSR ${baselines}/$id/${id}_BQSR.table \
         -I ${baselines}/$id/${id}_marked.bam \
         -o ${temp_dir}/${id}_final_BAM.bam \
-        -nct 8
+        -nct 1
 
 if [[ $? -ne 0 ]];then
 echo "Failed print reads"
 fi
 
+BAM=$($DIR/compare_BAM.sh ${temp_dir}/${id}_final_BAM.bam $id)
+
+fi
 #Remove Intermediate
 #rm $temp_dir/${id}_aligned.bam
 #rm $temp_dir/${id}_aligned.bam.bai
 
+if [[ $htc == 1 || $all == 1 ]];then
+
 #Haplotype Caller
-if [ -d "$baselines/$id/${id}_final_BAM.bam" ];then
-  num_proc=32
-  proc_id=0
-  for file in $(ls "$baselines/$id/${id}_final_BAM.bam")
-  do
-    if [[ $file =~ bam$ ]];then
-      java -Xmx4g -jar $gatk -T HaplotypeCaller \
-          -R $ref_genome \
-          -I ${baselines}/$id/${id}_final_BAM.bam/$file \
-          -o $temp_dir/${file}.vcf &
-
-      pid_table["$proc_id"]=$!
-      proc_id=$(($proc_id + 1))
-      if [ $proc_id -eq $num_proc ];then
-      #Wait for current tasks
-        for i in $(seq 0 $(($proc_id - 1)));do
-          wait "${pid_table["$i"]}"
-        done
-        proc_id=0
-      fi
-    fi
-  done
-  for i in $(seq 0 $(($proc_id - 1))); do
-    wait "${pid_table["$i"]}"
-  done
-  $VCF_CONCAT $temp_dir/${file}.vcf.gz | gzip > $temp_dir/${id}.vcf.gz
-
-#If single file
-elif [ -f "$baselines/$id/${id}_final_BAM.bam" ];then
-  java -jar $gatk -T HaplotypeCaller \
+java -jar $gatk -T HaplotypeCaller \
         -R $ref_genome \
         -I ${baselines}/$id/${id}_final_BAM.bam \
         -o $temp_dir/${id}.vcf \
-        -nct 8 
-fi
+        -nct 1 
 
 if [[ $? -ne 0 ]];then
   echo "Failed haplotype caller"
 fi
 
+VCF=$($DIR/compare_VCF.sh $temp_dir/${id}.vcf $id)
+
+fi
 #Remove Intermediate
 #rm -r $temp_dir/${id}_final_BAM.bam
 
-BQSR=$($DIR/compare_BQSR.sh ${temp_dir}/${id}_BQSR.table $id)
-BAM=$($DIR/compare_BAM.sh ${temp_dir}/${id}_final_BAM.bam $id)
-VCF=$($DIR/compare_VCF.sh $temp_dir/${id}.vcf $id)
-
-#echo "BQSR = $BQSR"
-#echo "BAM = $BAM"
-#echo "VCF = $VCF"
 echo "$id,$BQSR,$BAM,$VCF" >> $out
 done <$data_list
 
-
 #Copy to s3
-#aws s3 cp performance.csv s3://fcs-genome-data/benchmarks/${gatk_version}/performace.csv
-#aws s3 cp --recursive log/ s3://fcs-genome-data/benchmarks/${gatk_version}/log/
+#aws s3 cp $out s3://fcs-genome-data/results-validation/$gatk_version/record.csv
 
