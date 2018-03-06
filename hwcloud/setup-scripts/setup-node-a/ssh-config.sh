360bf706a94cc094ca975427475939fc2b0ba491
Di Wu <allwu@falcon-computing.com>
2018-03-06 09:07:43 +0800

    add setup script for 2-node image

---
+++ hwcloud/setup-scripts/setup-node-a/ssh-config.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+dest_ip=$1
+
+# create ssh key pairs
+mkdir -p ~/.ssh
+cd ~/.ssh
+ssh-keygen -f id_rsa -t rsa -N ''
+cat id_rsa.pub >> authorized_keys
+chmod 600 authorized_keys
+
+cat id_rsa.pub | ssh $dest_ip "
+  mkdir -p ~/.ssh; \
+  cd ~/.ssh; \
+  ssh-keygen -f id_rsa -t rsa -N ''; \
+  cat - >> authorized_keys; \
+  chmod 600 authorized_keys; \
+  cat id_rsa.pub" >> authorized_keys
+
+# create config
+touch ~/.ssh/config
+
+add_config() {
+  local line="$1";
+  local file="$2";
+  if [ -z "$(sudo grep "$line" $file)" ]; then
+    echo $line | tee --append $file;
+  fi;
+}
+
+add_config "StrictHostKeyChecking=no" ~/.ssh/config
+add_config "UserKnownHostsFile=/dev/null" ~/.ssh/config
+
+scp ~/.ssh/config $dest_ip:~/.ssh/
