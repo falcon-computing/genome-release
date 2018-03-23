f2c9d43bab0777988453cd5b842dc099d3def6f9
Di Wu <allwu@cs.ucla.edu>
2018-03-22 17:50:17 -0700

    Revert "Release package creating with testing"

--- test/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-for bat in $(ls $DIR/cases/*.bats); do
-  ${DIR}/bats/bin/bats $bat
-  if [ "$?" -ne 0 ]; then
-    exit 1
-  fi
-done
