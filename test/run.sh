a81dcb646bc6e0fffe625ecd5aa0d72f0554b77d
Niveda-S <nivedasundar05@gmail.com>
2018-05-02 12:06:58 -0700

    Add Validation summary report

--- test/run.sh
+++ test/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-echo -e "\n"
-echo "====================================================="
-echo "Testing Falcon Genome Release Package"
-echo "====================================================="
-echo -e "\n"
+echo -e "\n"  >> test.log
+echo "=====================================================" >> test.log
+echo "Testing Falcon Genome Release Package" >> test.log
+echo "=====================================================" >> test.log
+echo -e "\n" >> test.log
 
 
-echo -e "Begin Test\n"
+echo -e "Begin Test\n" >> test.log
 #for bat in $(ls $DIR/cases/*.bats); do
 #  ${DIR}/../bats/bats $bat
 #  if [ "$?" -ne 0 ]; then
@@ -22,7 +22,9 @@ aws s3 cp --recursive s3://fcs-genome-data/tools/vcfdiff/ vcfdiff
 
 start_ts=$(date +%s)
 
-${DIR}/../bats/bats results_test/
+${DIR}/../bats/bats results_test/ >> test.log
 
 end_ts=$(date +%s)
-echo "Time taken: $((end_ts - start_ts))s"
+echo "Time taken: $((end_ts - start_ts))s"  >> test.log
+
+aws sns publish --topic-arn arn:aws:sns:us-east-1:520870693817:Genomics_Pipeline_Results --subject "Results Validation: FROM ${HOSTNAME}" --message file://test.log
