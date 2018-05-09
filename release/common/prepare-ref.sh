e1e60ffc0f316e044f4327296f37a8a569077930
Di Wu <allwu@falcon-computing.com>
2018-05-09 11:14:53 -0700

    fixing prepare-ref.sh, and add fcs-genome.conf to version control

--- release/common/prepare-ref.sh
+++ release/common/prepare-ref.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-FALCON_DIR=/usr/local/falcon
-
-PICARD=$FALCON_DIR/tools/package/picard.jar
-BWA=$FALCON_DIR/tools/bin/bwa-org
-
-ref=$1
 if [ ! -f "$1" ]; then
 	echo "USAGE: $0 ref.fa"
 	exit 1
 fi
 
-set -x
-# prepare dict
-java -jar $PICARD CreateSequenceDictionary \
-	R=$ref \
-	O=${ref%%.fasta}.dict
+ref_genome=$1
+if [ ! -f "$ref_genome" ]; then
+  echo "Cannot find $ref_genome, skip preparation"
+  exit 1
+fi
 
-# prepare sa
-$BWA index $ref
-set +x
+FALCON_DIR=/usr/local/falcon
+PICARD=$FALCON_DIR/tools/package/picard.jar
+BWA=$FALCON_DIR/tools/bin/bwa-org
+ref_dict=${ref_genome%%.fasta}.dict
+ref_idx=${ref_genome}.fai
+ref_sa=${ref_genome}.sa
+ 
+if [ ! -f $ref_dict ]; then
+  java -jar $PICARD CreateSequenceDictionary \
+	R=$ref_genome \
+	O=$ref_dict
+fi
+
+if [ ! -f $ref_dict ]; then
+  $SAMTOOLS faidx $ref_genome
+fi
+
+if [ ! -f $ref_sa ]; then
+  $BWA index $ref_genome
+fi
+
+echo "Reference genome preparation is done"
