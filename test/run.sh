cffd6f2f6b59e701009fabd42f0416c3314716e1
Niveda-S <nivedasundar05@gmail.com>
2018-05-09 23:46:18 -0700

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -7,6 +7,18 @@ echo "Testing Falcon Genome Release Package" >> test.log
 echo "=====================================================" >> test.log
 echo -e "\n" >> test.log
 
+FALCON_DIR=/usr/local/falcon/
+
+FCSBIN=$FALCON_DIR/bin/fcs-genome
+BWABIN=$FALCON_DIR/tools/bin/bwa-bin
+GATK=$FALCON_DIR/tools/package/GenomeAnalysisTK.jar
+
+source $FALCON_DIR/setup.sh
+
+echo "$($FCSBIN | head -n1)" >> test.log
+echo "$($BWABIN --version)"  >> test.log
+echo "$($FCSBIN gatk --version | tail -n1)" >> test.log
+echo "$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep "instanceType" | awk 'BEGIN {FS=":"} {print $2}')" >> test.log
 
 echo -e "Begin Test\n" >> test.log
 #for bat in $(ls $DIR/cases/*.bats); do
