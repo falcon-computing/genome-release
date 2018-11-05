dd9576053676f4d919f8cfdf3190d386d7780a77
AriesLL <memoryzpp@gmail.com>
2018-11-05 14:02:05 -0800

    update kubectrl.sh for update yaml file name

--- docker/kubectrl.sh
+++ docker/kubectrl.sh
@@ -1,6 +1,6 @@
-kubectl create -f ./testbwa.yaml. #create the pod
-kubectl get pods --show-all #look up which pod is the testbwa, get the name of pod
+kubectl create -f ./fcs.yaml. #create the pod
+kubectl get pods --show-all #look up which pod is the fcs, get the name of pod
 
-kubectl logs testbwa-#id #output the logs of the pod
-kubectl delete jobs/testbwa #delete the pod
+kubectl logs fcs-#id #output the logs of the pod
+kubectl delete jobs/fcs #delete the pod
 
